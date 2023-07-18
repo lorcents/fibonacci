714b59e4ee1a22ea26188ee13730a022ba1fa8fb
innovex <iannjoroge120@gmail.com>
2023-07-18 21:45:48 +0300

    .

--- fibonacci.c
+++ fibonacci.c
@@ -1,65 +1,96 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "fibonacci.h"
 
-Node* makeConst(int value) {
+// Enum to represent arithmetic operations
+typedef enum {
+    CONSTANT,
+    ADD,
+    SUB,
+    MUL,
+    DIV
+} TypeTag;
+
+// Structure to represent a node in the expression tree
+typedef struct Node {
+    TypeTag type;
+    int value;
+    struct Node* left;
+    struct Node* right;
+} Node;
+
+// Function to create a new node with a constant value
+Node* makeConstant(int value) {
     Node* node = (Node*)malloc(sizeof(Node));
-    node->type = CONST;
+    node->type = CONSTANT;
     node->value = value;
     node->left = NULL;
     node->right = NULL;
     return node;
 }
 
-Node* makeFunc(TypeTag type) {
+// Function to create a new node with an arithmetic operation
+Node* makeFunc(TypeTag type, Node* left, Node* right) {
     Node* node = (Node*)malloc(sizeof(Node));
     node->type = type;
-    node->left = NULL;
-    node->right = NULL;
+    node->left = left;
+    node->right = right;
     return node;
 }
 
-void calc(Node* node) {
-    if (node->type == CONST) {
-        return;
-    }
-
-    if (node->type == ADD) {
-        calc(node->left);
-        calc(node->right);
-        node->value = addFunc(node->left->value, node->right->value);
-    } else if (node->type == SUB) {
-        calc(node->left);
-        calc(node->right);
-        node->value = subFunc(node->left->value, node->right->value);
-    } else if (node->type == MUL) {
-        calc(node->left);
-        calc(node->right);
-        node->value = mulFunc(node->left->value, node->right->value);
-    } else if (node->type == DIV) {
-        calc(node->left);
-        calc(node->right);
-        node->value = divFunc(node->left->value, node->right->value);
-    }
-}
-
-int addFunc(int a, int b) {
-    return a + b;
-}
-
-int subFunc(int a, int b) {
-    return a - b;
-}
-
-int mulFunc(int a, int b) {
-    return a * b;
-}
-
-int divFunc(int a, int b) {
-    if (b != 0) {
-        return a / b;
-    } else {
-        printf("Error: Division by zero.\n");
+// Function to evaluate the expression tree and return the result
+int evaluate(Node* node) {
+    if (node == NULL) {
         return 0;
     }
+
+    if (node->type == CONSTANT) {
+        return node->value;
+    }
+
+    int leftValue = evaluate(node->left);
+    int rightValue = evaluate(node->right);
+
+    switch (node->type) {
+        case ADD:
+            return leftValue + rightValue;
+        case SUB:
+            return leftValue - rightValue;
+        case MUL:
+            return leftValue * rightValue;
+        case DIV:
+            return leftValue / rightValue;
+        // Handle any other necessary cases here
+        default:
+            return 0;
+    }
+}
+
+// Function to handle memory deallocation of the expression tree
+void freeTree(Node* node) {
+    if (node == NULL) {
+        return;
+    }
+    freeTree(node->left);
+    freeTree(node->right);
+    free(node);
+}
+
+int main() {
+    Node* add = makeFunc(ADD, makeConstant(10), makeConstant(6));
+    Node* mul = makeFunc(MUL, makeConstant(5), makeConstant(4));
+    Node* sub = makeFunc(SUB, mul, add);
+    Node* fibo = makeFunc(SUB, sub, NULL);
+
+    printf("add : %d\n", evaluate(add));
+    printf("mul : %d\n", evaluate(mul));
+    printf("sub : %d\n", evaluate(sub));
+    printf("fibo : %d\n", evaluate(fibo));
+
+    // Free allocated memory
+    freeTree(add);
+    freeTree(mul);
+    freeTree(sub);
+    freeTree(fibo);
+
+    return 0;
 }
--- readme.md
+++ readme.md
@@ -2,11 +2,8 @@
 
 ## Implementation Details
 
-The project includes the following files:
+The  include fibonacci.c  and Readme md
 
-- `fibonacci.h`: The header file containing function declarations and necessary structures.
-- `fibonacci.c`: The source file containing the implementations of the functions declared in `fibonacci.h`.
-- `main.c`: The main function to test the Fibonacci sequence using different approaches.
 
 ## Development Environment
 
@@ -19,11 +16,17 @@ The project was developed using the following environment:
 
 To compile and run the source code, follow these steps:
 
-1. Make sure all the source files (`fibonacci.h`, `fibonacci.c`, and `main.c`) are  in the same directory.
+1. Clone the repo
 
-2. Open a terminal or command prompt and navigate to the directory where the source files are located.
+2. Open a terminal or command prompt and navigate to the directory where the source file is located.
 
 3. Compile the source files using GCC:
 
    ```bash
-   gcc -o fibonacci main.c fibonacci.c
+   gcc -o fibonacci  fibonacci.c
+
+4. Run:
+
+   ```bash
+   ./fibonacci
+
